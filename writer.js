b86e1fd6a263f2e03abfa2db0fb6fa782587cce9
C. Heisch <chrickenh@gmail.com>
2024-09-30 15:31:36 +0200

    port 1084

--- writer.js
+++ writer.js
@@ -15,7 +15,7 @@ server.use(routes);
 const init = () => {
     db.init().then(
         () => {
-            server.listen(1080, err => {
+            server.listen(1084, err => {
                 if (err) console.log(err);
                 else console.log('Server läuft');
             })
